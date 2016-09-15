ef3533cc72dd1eed1199ae9da6c1d34e8e1cfcce
Evildavo <evildavo@gmail.com>
2016-09-15 23:24:55 +1000

    Time acceleration can be controlled with a boolean switch

--- SGD103-Homeless/Assets/Scripts/GameTime.cs
+++ SGD103-Homeless/Assets/Scripts/GameTime.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameTime : MonoBehaviour {
-
+public class GameTime : MonoBehaviour
+{
+    // Settings.
+    public float NormalTimeScale = 1.0f;
+    public float AcceleratedTimeScale = 1.0f;
+    public bool IsTimeAccelerated = false;
     public float TimeScale = 1.0f;
-
-	void Start () {
+    
+    void Start () {
 	
 	}
 	
 	void Update () {
-	
-	}
+        if (IsTimeAccelerated) { TimeScale = AcceleratedTimeScale; }
+        else { TimeScale = NormalTimeScale; }
+    }
 }
