5ebe4eb4dbc234d53d0c072502f53cf78c15e4b0
Evildavo <evildavo@gmail.com>
2016-09-16 00:43:38 +1000

    Sun angle changes with respect to the time of day

---
+++ SGD103-Homeless/Assets/Scripts/Sun.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sun : MonoBehaviour {
+
+    public GameTime GameTime;
+    
+	void Start () {
+	
+	}
+	
+	void Update () {
+        var SUNRISE_AT_HOUR = 6.0f;
+
+        var sunXRotationDegrees = (GameTime.TimeOfDayHours - SUNRISE_AT_HOUR) / 24.0f * 360.0f;
+        transform.rotation = Quaternion.AngleAxis(sunXRotationDegrees, Vector3.left);
+    }
+}
