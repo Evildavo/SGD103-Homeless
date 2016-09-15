263ab35aac41819f54d455933985f4a97e4a8b2f
Evildavo <evildavo@gmail.com>
2016-09-15 23:01:23 +1000

    Hunger increases steadily over time up to a maximum of 100%

--- SGD103-Homeless/Assets/Scripts/PlayerState.cs
+++ SGD103-Homeless/Assets/Scripts/PlayerState.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class PlayerState : MonoBehaviour {
 
-	// Use this for initialization
+    [Range(0.0f, 1.0f)]
+    public float Hunger = 0;
+
+    public float HungerIncreasePerSecond;
+    
 	void Start () {
 	
 	}
 	
-	// Update is called once per frame
 	void Update () {
-	
-	}
+        Hunger += HungerIncreasePerSecond * Time.deltaTime;
+        if (Hunger > 1.0f)
+        {
+            Hunger = 1.0f;
+        }
+    }
 }
