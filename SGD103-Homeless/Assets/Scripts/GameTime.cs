bad37efebe7484bf556ef7c21ae39b12aa9abf64
Evildavo <evildavo@gmail.com>
2016-09-16 00:05:54 +1000

    Time of day wraps and progresses day count

--- SGD103-Homeless/Assets/Scripts/GameTime.cs
+++ SGD103-Homeless/Assets/Scripts/GameTime.cs
@@ -7,6 +7,7 @@ public class GameTime : MonoBehaviour
     public float AcceleratedTimeScale = 1.0f;
     public bool IsTimeAccelerated = false;
 
+    public int Day = 1;
     [Range(0.0f, 24.0f)]
     public float TimeOfDayHours = 0.0f;
     public float TimeScale = 1.0f;
@@ -25,5 +26,10 @@ public class GameTime : MonoBehaviour
             TimeScale = NormalTimeScale;
         }
         TimeOfDayHours += Time.deltaTime / 60.0f / 60.0f * TimeScale;
+        if (TimeOfDayHours >= 24.0f)
+        {
+            TimeOfDayHours = 0.0f;
+            Day += 1;
+        }
     }
 }
