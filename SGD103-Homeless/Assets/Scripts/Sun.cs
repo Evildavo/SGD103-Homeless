1fc5adc95365ea83a56407a560ea645b44214652
Evildavo <evildavo@gmail.com>
2016-09-16 00:50:49 +1000

    Sun rises from the East

--- SGD103-Homeless/Assets/Scripts/Sun.cs
+++ SGD103-Homeless/Assets/Scripts/Sun.cs
@@ -1,18 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Sun : MonoBehaviour {
+public class Sun : MonoBehaviour
+{
 
     public GameTime GameTime;
-    
-	void Start () {
-	
-	}
-	
-	void Update () {
-        var SUNRISE_AT_HOUR = 6.0f;
 
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        // Calculate sun angle.
+        var SUNRISE_AT_HOUR = 6.0f;
         var sunXRotationDegrees = (GameTime.TimeOfDayHours - SUNRISE_AT_HOUR) / 24.0f * 360.0f;
-        transform.rotation = Quaternion.AngleAxis(sunXRotationDegrees, Vector3.left);
+        var sunYRotationDegrees = 180.0f;
+        transform.rotation =
+            Quaternion.AngleAxis(sunXRotationDegrees, Vector3.left) *
+            Quaternion.AngleAxis(sunYRotationDegrees, Vector3.up);
     }
 }
