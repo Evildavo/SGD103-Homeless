ca8e1520fd241a8b818f3375b3b835e88fc28378
David Page <dcp002@usc.edu.au>
2016-09-26 14:56:23 +1000

    Created test environment for movement with invisible collision meshes

---
+++ SGD103-Homeless/Assets/Scripts/CollisionMesh.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionMesh : MonoBehaviour {
+    
+	void Start () {
+        GetComponent<Renderer>().enabled = false;
+	}
+	
+	void Update () {
+	
+	}
+}
