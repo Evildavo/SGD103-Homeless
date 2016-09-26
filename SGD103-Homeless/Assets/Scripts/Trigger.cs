8f71eaf0a04101cbc085de7580e845b39edaf226
David Page <dcp002@usc.edu.au>
2016-09-26 16:53:33 +1000

    Trigger is triggered on a key press

---
+++ SGD103-Homeless/Assets/Scripts/Trigger.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Trigger : MonoBehaviour
+{
+    private bool isNearby = false;
+
+    public bool IsActive = true;
+    public Text InteractHintText;
+
+    void OnTriggerEnter(Collider other)
+    {
+        isNearby = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        isNearby = false;
+    }
+
+    void Update()
+    {
+        if (InteractHintText)
+        {
+            if (IsActive && isNearby)
+            {
+                InteractHintText.GetComponent<Text>().enabled = true;
+            }
+            else
+            {
+                InteractHintText.GetComponent<Text>().enabled = false;
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        if (IsActive && isNearby && Event.current.keyCode == KeyCode.E)
+        { 
+            IsActive = false;
+        }
+    }
+
+}
