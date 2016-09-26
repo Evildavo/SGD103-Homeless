198ae42b0a42ea57f827e7ff1f73c8056b049f34
David Page <dcp002@usc.edu.au>
2016-09-26 16:14:35 +1000

    Interaction hint text displays when in the trigger zone

---
+++ SGD103-Homeless/Assets/TestTrigger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TestTrigger : MonoBehaviour {
+
+    public Text InteractHintText;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (InteractHintText)
+        {
+            InteractHintText.GetComponent<Text>().enabled = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (InteractHintText)
+        {
+            InteractHintText.GetComponent<Text>().enabled = false;
+        }
+    }
+
+    void Start () {
+	    
+	}
+	
+	void Update () {
+	
+	}
+}
