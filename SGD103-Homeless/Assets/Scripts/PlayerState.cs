549eef4f9c34d2777d1dccb41a5205b8266054c7
David Page <dcp002@usc.edu.au>
2016-09-26 13:55:30 +1000

    Added health, hunger/thirst, money and morale to player state

--- SGD103-Homeless/Assets/Scripts/PlayerState.cs
+++ SGD103-Homeless/Assets/Scripts/PlayerState.cs
@@ -4,20 +4,38 @@ using System.Collections;
 public class PlayerState : MonoBehaviour {
 
     public GameTime GameTime;
+    
+    public float Money = 0;
 
     [Range(0.0f, 1.0f)]
-    public float Hunger = 0;
+    public float HungerThirst = 1.0f;
+    [Range(0.0f, 1.0f)]
+    public float Health = 1.0f;
+    [Range(0.0f, 1.0f)]
+    public float Morale = 1.0f;
     public float HungerIncreasePerGameHour;
-    
-	void Start () {
+    public float HealthDecreasePerGameHour;
+    public float MoraleDecreasePerGameHour;
+
+    void Start () {
 	
 	}
 	
 	void Update () {
-        Hunger += HungerIncreasePerGameHour / 60.0f / 60.0f * Time.deltaTime * GameTime.TimeScale;
-        if (Hunger > 1.0f)
+        HungerThirst -= HungerIncreasePerGameHour / 60.0f / 60.0f * Time.deltaTime * GameTime.TimeScale;
+        if (HungerThirst > 1.0f)
         {
-            Hunger = 1.0f;
+            HungerThirst = 1.0f;
+        }
+        Health -= HealthDecreasePerGameHour / 60.0f / 60.0f * Time.deltaTime * GameTime.TimeScale;
+        if (Health > 1.0f)
+        {
+            Health = 1.0f;
+        }
+        Morale -= MoraleDecreasePerGameHour / 60.0f / 60.0f * Time.deltaTime * GameTime.TimeScale;
+        if (Morale > 1.0f)
+        {
+            Morale = 1.0f;
         }
     }
 }
