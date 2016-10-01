5caa07be2a5c7a759d8a6fe91e0fc3024c9a8483
Evildavo <evildavo@gmail.com>
2016-10-01 23:31:17 +1000

    Opening watch displays the current game time

--- SGD103-Homeless/Assets/Scripts/MessageBox.cs
+++ SGD103-Homeless/Assets/Scripts/MessageBox.cs
@@ -1,11 +1,32 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class MessageBox : MonoBehaviour {
-    
-	void Start () {
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
         gameObject.SetActive(false);
-	}
+    }
+
+    public bool IsDisplayed()
+    {
+        return gameObject.activeInHierarchy;
+    }
+
+    public void SetMessage(string message)
+    {
+        GetComponentInChildren<Text>().text = message;
+    }
+    
+    void Start () {
+        Hide();
+    }
 	
 	void Update () {
 	
--- SGD103-Homeless/Assets/Scripts/Test/InventoryItemTest1.cs
+++ SGD103-Homeless/Assets/Scripts/Test/InventoryItemTest1.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class InventoryItemTest1 : InventoryItem
 {
+    public MessageBox MessageBox;
+    public GameTime GameTime;
 
     public override void OnPrimaryAction()
     {
+        MessageBox.Show();
     }
 
     void Start () {
@@ -13,7 +16,12 @@ public class InventoryItemTest1 : InventoryItem
 	}
 	
 	void Update () {
-	
-	}
+
+        // Update watch.
+        if (MessageBox.IsDisplayed())
+        {
+            MessageBox.SetMessage("The time is " + GameTime.GetTimeAsString());
+        }
+    }
 
 }
