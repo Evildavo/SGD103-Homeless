1eaefe313cdc7197841c076e6d085e1377a53f56
David Page <dcp002@usc.edu.au>
2016-10-01 14:53:47 +1000

    Created test scene for inventory

---
+++ SGD103-Homeless/Assets/Scripts/TextStartsDisabled.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextStartsDisabled : MonoBehaviour {
+    
+	void Start () {
+        GetComponent<Text>().enabled = false;
+	}
+}
