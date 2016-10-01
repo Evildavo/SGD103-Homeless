cc0a1b522c9ae8c1a567c57d909bf475d8be03b4
Evildavo <evildavo@gmail.com>
2016-10-01 23:53:32 +1000

    Watch closes after a few seconds

--- SGD103-Homeless/Assets/Scripts/Test/WatchItemTest.cs
+++ SGD103-Homeless/Assets/Scripts/Test/WatchItemTest.cs
@@ -3,24 +3,31 @@ using System.Collections;
 
 public class WatchItemTest : InventoryItem
 {
+    private float timeAtActivateSeconds;
+
     public MessageBox MessageBox;
     public GameTime GameTime;
+
+    public float CloseAfterSeconds = 1.8f;
 
     public override void OnPrimaryAction()
     {
         MessageBox.Show();
+        timeAtActivateSeconds = Time.time;
     }
-
-    void Start () {
-	
-	}
 	
 	void Update () {
-
+        
         // Update watch.
         if (MessageBox.IsDisplayed())
         {
             MessageBox.SetMessage("The time is " + GameTime.GetTimeAsString());
+
+            // Close after some time.
+            if (Time.time - timeAtActivateSeconds > CloseAfterSeconds)
+            {
+                MessageBox.Hide();
+            }
         }
     }
 
