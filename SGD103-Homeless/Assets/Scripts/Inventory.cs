898827a9991104be5ea6e2b428dadf91762fe815
David Page <dcp002@usc.edu.au>
2016-10-03 10:45:46 +1000

    Inventory hides after time and is woken by mouse movement

--- SGD103-Homeless/Assets/Scripts/Inventory.cs
+++ SGD103-Homeless/Assets/Scripts/Inventory.cs
@@ -2,8 +2,12 @@
 using System.Collections;
 
 public class Inventory : MonoBehaviour {
+    private bool isAwake = true;
+    private float timeAtWake;
 
     public Transform InventorySlotContainer;
+
+    public float HideAfterSeconds = 4.0f;
 
     public InventorySlot[] GetSlots()
     {
@@ -13,8 +17,29 @@ public class Inventory : MonoBehaviour {
 	void Start () {
 	    
 	}
-	
-	void Update () {
-	
-	}
+
+    void Update()
+    {
+        // If mouse moved wake up inventory.
+        if (Input.GetAxis("Mouse X") != 0 && Input.GetAxis("Mouse Y") != 0)
+        {
+            isAwake = true;
+            timeAtWake = Time.time;
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(true);
+            }
+        }
+
+        // After time hide the inventory.
+        if (isAwake && Time.time - timeAtWake > HideAfterSeconds)
+        {
+            isAwake = false;
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+    }
+
 }
