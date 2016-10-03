1d944a18f691cd5392b3f1310681120a4510431f
David Page <dcp002@usc.edu.au>
2016-10-03 10:22:11 +1000

    Inventory is now an independent object from player

---
+++ SGD103-Homeless/Assets/Scripts/Inventory.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour {
+
+    public Transform InventorySlotContainer;
+
+    public InventorySlot[] GetSlots()
+    {
+        return InventorySlotContainer.GetComponentsInChildren<InventorySlot>();
+    }
+    
+	void Start () {
+	    
+	}
+	
+	void Update () {
+	
+	}
+}
