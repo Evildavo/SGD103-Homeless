f214f2028a20b9ca05605f62e981e4756f28f014
David Page <evildavo@gmail.com>
2016-10-06 12:30:47 +1000

    Test scene for menu system

---
+++ SGD103-Homeless/Assets/Scripts/Test/MenuTriggerTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuTriggerTest : Trigger {
+    
+    public override void OnTrigger()
+    {
+        IsActive = true;
+    }
+
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+
+    public override void OnTriggerEnter(Collider other)
+    {
+        base.OnTriggerEnter(other);
+    }
+
+    public override void OnTriggerExit(Collider other)
+    {
+        base.OnTriggerExit(other);
+    }
+}
