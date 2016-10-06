8fa41365d8d9e7da5bd866dedaeade98f2c1a90d
David Page <evildavo@gmail.com>
2016-10-06 13:21:50 +1000

    Added zone

---
+++ SGD103-Homeless/Assets/Scripts/Zone.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Zone : MonoBehaviour
+{
+    public string ZoneName;
+    
+    // Call from derived.
+    public virtual void Start()
+    {
+        GetComponent<Renderer>().enabled = false;
+    }
+
+    // Call from derived.
+    public virtual void Update() {}
+
+    // Override to do some action on entering.
+    public virtual void OnTriggerEnter(Collider other) {}
+
+    // Override to do some action on exiting.
+    public virtual void OnTriggerExit(Collider other) {}
+
+}
