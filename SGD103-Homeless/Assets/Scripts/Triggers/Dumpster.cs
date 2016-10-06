bf29c4ec2b2a014895c29d08c0c67a657894a82d
Evildavo <evildavo@gmail.com>
2016-10-06 15:55:30 +1000

    Test dumpster code

---
+++ SGD103-Homeless/Assets/Scripts/Triggers/Dumpster.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dumpster : Trigger {
+    private float timeAtLastCheck;
+
+    public PlayerState PlayerState;
+
+    public bool Searching = false;
+
+    public override void OnTrigger()
+    {
+        Searching = true;
+        timeAtLastCheck = Time.time;
+    }
+
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (Time.time - timeAtLastCheck > 1)
+        {
+            float value = Random.Range(0, 100);
+            if (value < 10)
+            {
+                PlayerState.HungerThirst += 0.2f;
+                PlayerState.HungerThirst -= 0.2f;
+            }
+        }
+    }
+
+    public override void OnTriggerEnter(Collider other)
+    {
+        base.OnTriggerEnter(other);
+    }
+
+    public override void OnTriggerExit(Collider other)
+    {
+        base.OnTriggerExit(other);
+        Searching = false;
+        IsActive = true;
+    }
+}
