70595641d175686468b57dee8f1a49aae8276647
David Page <dcp002@usc.edu.au>
2016-10-07 14:01:10 +1000

    Background cars (incomplete because had to leave room)

---
+++ SGD103-Homeless/Assets/Scripts/CarBackground.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarBackground : MonoBehaviour {
+
+    public float Speed = 6.0f;
+    public Zone WrapFrom;
+    public Zone WrapTo;
+
+    void Start () {
+	
+	}
+	
+	void Update () {
+
+        // Move forward.
+        transform.Translate(new Vector3(Speed, 0.0f, 0.0f) * Time.deltaTime);
+	}
+    
+    public void OnTriggerEnter(Collider other)
+    {
+        // Wrap over when running out of road.
+        if (other.gameObject == WrapFrom.gameObject)
+        {
+            Transform delta = transform;
+            delta.Translate(-WrapTo.transform.position);
+            delta.Rotate(new Vector3(0.0f, transform.eulerAngles.y, 0.0f));
+            Vector3 position = delta.position;
+            position.x = 0.0f;
+            delta.position = position;
+            delta.Translate(WrapTo.transform.position);
+            transform.position = delta;
+        }
+    }
+}
