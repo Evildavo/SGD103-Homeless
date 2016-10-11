9484dd550d68f9df6a598b1d36aaade7aef7e9b1
Evildavo <evildavo@gmail.com>
2016-10-11 13:34:15 +1000

    Time accelerates while searching for garbage. Once found the user is prompted to eat it or not.

--- SGD103-Homeless/Assets/Scripts/Triggers/Dumpster.cs
+++ SGD103-Homeless/Assets/Scripts/Triggers/Dumpster.cs
@@ -2,32 +2,42 @@
 using System.Collections;
 
 public class Dumpster : MonoBehaviour {
+    private float timeAtLastCheck;
 
     public Trigger Trigger;
     public PlayerState PlayerState;
     public ConfirmationBox ConfirmationBox;
+
+    public float ChanceOfFindingFoodPerSecond = 0.1f;
     
     public void OnTrigger()
     {
+        timeAtLastCheck = Time.time;
     }
 
     public void OnTriggerUpdate()
     {
-        float value = Random.Range(0, 10);
-        if (value == 0)
+        // Every second, randomly decide if we've found food.
+        if (Time.time - timeAtLastCheck > 1.0f)
         {
-            ConfirmationBox.OnConfirmation onEatFoodConfirmed = () =>
+            float value = Random.Range(0.0f, 1.0f);
+            if (value <= ChanceOfFindingFoodPerSecond)
             {
-                PlayerState.HungerThirst += 0.2f;
-                PlayerState.Health -= 0.2f;
-            };
-            ConfirmationBox.Open(onEatFoodConfirmed, "You found food. Eat it?", "Yes", "No");
-            Trigger.Reset();
+                ConfirmationBox.OnConfirmation onEatFoodConfirmed = () =>
+                {
+                    PlayerState.HungerThirst += 0.2f;
+                    PlayerState.Health -= 0.2f;
+                };
+                ConfirmationBox.Open(onEatFoodConfirmed, "You found food. Eat it?", "Yes", "No");
+                Trigger.Reset();
+            }
+            timeAtLastCheck = Time.time;
         }
     }
     
     public void OnPlayerExit()
     {
+        ConfirmationBox.Close();
         Trigger.Reset();
     }
 }
