37e731b099b3b498ff9c8e9cb7f923d7583bd4dd
Evildavo <evildavo@gmail.com>
2016-10-12 11:21:59 +1000

    Added library trigger menu

---
+++ SGD103-Homeless/Assets/Scripts/Triggers/Library.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Library : MonoBehaviour {
+    private Trigger trigger;
+
+    public Menu Menu;
+    public List<Menu.Option> Options;
+
+    public void OnTrigger(Trigger trigger)
+    {
+        this.trigger = trigger;
+        Menu.Show(Options);
+    }
+
+    public void OnJobSearch(string name, int value)
+    {
+        Debug.Log("Searching for job");
+    }
+
+    public void OnReadBooks(string name, int value)
+    {
+        Debug.Log("Reading books");
+    }
+
+    public void OnExit(string name, int value)
+    {
+        Menu.Hide();
+        trigger.Reset();
+    }
+
+    public void OnPlayerExit(Trigger trigger)
+    {
+        Menu.Hide();
+        trigger.Reset();
+    }
+    
+}
