b505757255be2beec35d1f88ee58733083de1c3a
Evildavo <evildavo@gmail.com>
2016-10-12 12:02:15 +1000

    Reading books improves morale

--- SGD103-Homeless/Assets/Scripts/Triggers/Library.cs
+++ SGD103-Homeless/Assets/Scripts/Triggers/Library.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 
 public class Library : MonoBehaviour {
     private Trigger trigger;
+    private bool isReading;
 
     public Menu Menu;
-    public List<Menu.Option> Options;
+    public MessageBox MessageBox;
+    public PlayerState PlayerState;
+    public List<Menu.Option> MainMenu;
+    public List<Menu.Option> ReadingMenu;
+
+    public float MoraleGainedPerSecond = 0.05f;
 
     public void OnTrigger(Trigger trigger)
     {
         this.trigger = trigger;
-        Menu.Show(Options);
+        Menu.Show(MainMenu);
     }
 
     public void OnJobSearch(string name, int value)
@@ -20,7 +26,12 @@ public class Library : MonoBehaviour {
 
     public void OnReadBooks(string name, int value)
     {
-        Debug.Log("Reading books");
+        MessageBox.SetMessage("You are reading...");
+        MessageBox.Show(gameObject);
+        Menu.Show(ReadingMenu);
+        PlayerState.HighlightMorale = true;
+        trigger.GameTime.IsTimeAccelerated = true;
+        isReading = true;
     }
 
     public void OnExit(string name, int value)
@@ -29,10 +40,42 @@ public class Library : MonoBehaviour {
         trigger.Reset();
     }
 
-    public void OnPlayerExit(Trigger trigger)
+    public void OnStopReading(string name, int value)
+    {
+        Menu.Show(MainMenu);
+        MessageBox.Hide();
+        PlayerState.HighlightMorale = false;
+        trigger.GameTime.IsTimeAccelerated = false;
+        isReading = false;
+    }
+
+    void leaveTrigger()
     {
         Menu.Hide();
         trigger.Reset();
+        MessageBox.Hide();
+        OnStopReading("", 0);
+    }
+
+
+    public void OnPlayerExit(Trigger trigger)
+    {
+        leaveTrigger();
+    }
+
+    public void OnTriggerUpdate(Trigger trigger)
+    {
+        // Increase morale while reading.
+        if (isReading)
+        {
+            PlayerState.Morale += MoraleGainedPerSecond * Time.deltaTime;
+        }
+
+        // Leave menu on E key.
+        if (Input.GetKeyDown("e") || Input.GetKeyDown("enter") || Input.GetKeyDown("return"))
+        {
+            leaveTrigger();
+        }
     }
     
 }
