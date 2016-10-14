a1e5dc657f408ea065d6d764b7b76d94890da41c
Evildavo <evildavo@gmail.com>
2016-10-14 21:25:48 +1000

    Draws using more efficient method now

--- SGD103-Homeless/Assets/Scripts/WriteYourSign.cs
+++ SGD103-Homeless/Assets/Scripts/WriteYourSign.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System;
 using System.Collections.Generic;
 
 public class WriteYourSign : MonoBehaviour {
@@ -28,48 +29,92 @@ public class WriteYourSign : MonoBehaviour {
         canvas.SetPixels(pixelArray);
         canvas.Apply();
     }
-
-    void drawBrush(int x, int y, Color colour)
-    {
-        if (x < CanvasWidthPixels && y < CanvasHeightPixels && x >= 0 && y >= 0)
-        {
-            canvas.SetPixel(x, y, colour);
-        }
-    }
 	
 	void Update ()
     {
         // Draw on the canvas if the mouse is down.
         if (Input.GetButton("Primary"))
         {
-            // Find current position.
-            Vector2 localPoint;
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(
-                GetComponent<RectTransform>(), Input.mousePosition, null, out localPoint);
-            int x = (int)(localPoint.x / GetComponent<RectTransform>().rect.width * CanvasWidthPixels);
-            int y = (int)(localPoint.y / GetComponent<RectTransform>().rect.height * CanvasHeightPixels);
+            // Find cursor position on canvas.
+            Vector2 cursorPosition;
+            {
+                Vector2 localPoint;
+                RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                    GetComponent<RectTransform>(), Input.mousePosition, null, out localPoint);
+                cursorPosition = new Vector2(
+                    localPoint.x / GetComponent<RectTransform>().rect.width * CanvasWidthPixels,
+                    localPoint.y / GetComponent<RectTransform>().rect.height * CanvasHeightPixels);
+            }
 
-            // Colour the canvas at that point.
-            drawBrush(x, y, PenColour);
+            // Capture the pixel area that we'll draw into.
+            Rect drawAreaRect;
+            Color[] drawAreaPixels;
+            {
+                if (hasLastPoint)
+                {
+                    Vector2 firstCorner = cursorPosition;
+                    Vector2 secondCorner = lastPoint;
+                    bool flipX = (secondCorner.x < firstCorner.x);
+                    bool flipY = (secondCorner.y < firstCorner.y);
+                    if (flipX)
+                    {
+                        float x = firstCorner.x;
+                        firstCorner.x = secondCorner.x;
+                        secondCorner.x = x;
+                    }
+                    if (flipY)
+                    {
+                        float y = firstCorner.y;
+                        firstCorner.y = secondCorner.y;
+                        secondCorner.y = y;
+                    }
+                    drawAreaRect = new Rect(firstCorner, secondCorner - firstCorner + new Vector2(1, 1));
+                    if (drawAreaRect.width < 1.0f)
+                    {
+                        drawAreaRect.width = 1.0f;
+                    }
+                    if (drawAreaRect.height < 1.0f)
+                    {
+                        drawAreaRect.height = 1.0f;
+                    }
+                }
+                else
+                {
+                    drawAreaRect = new Rect(cursorPosition, new Vector2(1, 1));
+                }
+                drawAreaPixels = canvas.GetPixels(
+                    Mathf.FloorToInt(drawAreaRect.x), Mathf.FloorToInt(drawAreaRect.y),
+                    Mathf.FloorToInt(drawAreaRect.width), Mathf.FloorToInt(drawAreaRect.height));
+            }
 
-            // Also draw points at spaces between the last point and this point.
+            // Anonymous function for drawing the brush at the given point.
+            Action<Vector2> drawBrush = (Vector2 point) =>
+            {
+                Vector2 pointInArea = point - drawAreaRect.position;
+                drawAreaPixels[(int)pointInArea.y * (int)drawAreaRect.width + (int)pointInArea.x] = PenColour;
+            };
+            
+            // Paint the canvas at the cursor.
+            drawBrush(cursorPosition);
+
+            // Also draw strokes to fill the space between the last point and this point.
+            Vector2 delta = cursorPosition - lastPoint;
             if (hasLastPoint)
             {
-                Vector2 delta = new Vector2(x - lastPoint.x, y - lastPoint.y);
                 int nPointsToAdd = Mathf.CeilToInt(delta.magnitude / PixelSpacing);
                 for (var i = 0; i < nPointsToAdd; i++)
                 {
-                    drawBrush(
-                        Mathf.FloorToInt(lastPoint.x + i * delta.normalized.x * PixelSpacing),
-                        Mathf.FloorToInt(lastPoint.y + i * delta.normalized.y * PixelSpacing), 
-                        PenColour);
+                    drawBrush(lastPoint + i * delta.normalized * PixelSpacing);
                 }
             }
 
-            // Apply the changes to the texture.
+            // Apply changes to the draw area and the texture.
+            canvas.SetPixels(
+                Mathf.FloorToInt(drawAreaRect.x), Mathf.FloorToInt(drawAreaRect.y),
+                Mathf.FloorToInt(drawAreaRect.width), Mathf.FloorToInt(drawAreaRect.height), 
+                drawAreaPixels);
             canvas.Apply();
-            lastPoint.x = x;
-            lastPoint.y = y;
+            lastPoint = cursorPosition;
             hasLastPoint = true;
         }
         else
