c7228259e915b428f0f2ad43059d979c2e8b8cf1
Evildavo <evildavo@gmail.com>
2016-10-16 17:50:33 +1000

    Mouse over sleep button gives sleep quality hint (with colour-coding)

--- SGD103-Homeless/Assets/Scripts/HUD Buttons/SleepHudButton.cs
+++ SGD103-Homeless/Assets/Scripts/HUD Buttons/SleepHudButton.cs
@@ -2,16 +2,23 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class SleepHudButton : MonoBehaviour {
+public class SleepHudButton : MonoBehaviour
+{
+    private bool isMouseOver = false;
 
     public Inventory Inventory;
     public Transform HudButtonLabel;
     public PlayerSleepManager SleepManager;
+    public bool GiveSleepHint = true;
+    public Color PoorSleepLabelColour = Color.white;
+    public Color OkSleepLabelColour = Color.white;
+    public Color GoodSleepLabelColour = Color.white;
 
     public void OnPointerEnter()
     {
+        isMouseOver = true;
         HudButtonLabel.gameObject.SetActive(true);
-        HudButtonLabel.GetComponentInChildren<Text>().text = "Sleep here";
+        HudButtonLabel.GetComponentInChildren<Text>().text = "";
         Vector3 position = HudButtonLabel.transform.position;
         position.x = transform.position.x;
         HudButtonLabel.transform.position = position;
@@ -19,12 +26,43 @@ public class SleepHudButton : MonoBehaviour {
 
     public void OnPointerExit()
     {
+        isMouseOver = false;
         HudButtonLabel.gameObject.SetActive(false);
+        HudButtonLabel.GetComponentInChildren<Text>().color = Color.white;
     }
 
     public void OnClick()
     {
         SleepManager.Sleep();
+    }
+
+    public void Update()
+    {
+        // Update the button label based on expected quality of sleep here.
+        if (isMouseOver)
+        {
+            HudButtonLabel.GetComponentInChildren<Text>().text = "Sleep here";
+            HudButtonLabel.GetComponentInChildren<Text>().color = Color.white;
+            
+            if (GiveSleepHint)
+            {
+                switch (SleepManager.SleepQualityHere)
+                {
+                    case PlayerSleepManager.SleepQualityEnum.POOR:
+                        HudButtonLabel.GetComponentInChildren<Text>().text += " (bad)";
+                        HudButtonLabel.GetComponentInChildren<Text>().color = PoorSleepLabelColour;
+                        break;
+                    case PlayerSleepManager.SleepQualityEnum.OK:
+                        HudButtonLabel.GetComponentInChildren<Text>().text += " (ok)";
+                        HudButtonLabel.GetComponentInChildren<Text>().color = OkSleepLabelColour;
+                        break;
+                    case PlayerSleepManager.SleepQualityEnum.GOOD:
+                        HudButtonLabel.GetComponentInChildren<Text>().text += " (good)";
+                        HudButtonLabel.GetComponentInChildren<Text>().color = GoodSleepLabelColour;
+                        break;
+                }
+            }
+        }
     }
 
 }
