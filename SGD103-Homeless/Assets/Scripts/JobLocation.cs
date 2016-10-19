5003f58d2eff645cf53099d1eee4f015de4de688
Evildavo <evildavo@gmail.com>
2016-10-20 01:23:14 +1000

    Backed out of having an option to not include days between the two given

--- SGD103-Homeless/Assets/Scripts/JobLocation.cs
+++ SGD103-Homeless/Assets/Scripts/JobLocation.cs
@@ -32,7 +32,6 @@ public class JobLocation : MonoBehaviour {
         public float ChanceOfSuccessWithResume;
         public GameTime.DayOfTheWeek WorkFromDay;
         public GameTime.DayOfTheWeek WorkToDay;
-        public bool WorkDaysInBetween = true;
         [Header("Note: Supports wrapping over (e.g. 11pm to 2am)")]
         public float ShiftFromHour;
         public float ShiftToHour;
@@ -50,33 +49,18 @@ public class JobLocation : MonoBehaviour {
         public void Calculate()
         {
             // Calculate the number of days worked per week.
-            if (WorkDaysInBetween)
+            DaysWorkPerWeek = 1;
+            GameTime.DayOfTheWeek dayOfTheWeek = WorkFromDay;
+            while (dayOfTheWeek != WorkToDay && DaysWorkPerWeek < 7)
             {
-                DaysWorkPerWeek = 1;
-                GameTime.DayOfTheWeek dayOfTheWeek = WorkFromDay;
-                while (dayOfTheWeek != WorkToDay && DaysWorkPerWeek < 7)
+                DaysWorkPerWeek++;
+                if (dayOfTheWeek == GameTime.DayOfTheWeek.SUNDAY)
                 {
-                    DaysWorkPerWeek++;
-                    if (dayOfTheWeek == GameTime.DayOfTheWeek.SUNDAY)
-                    {
-                        dayOfTheWeek = GameTime.DayOfTheWeek.MONDAY;
-                    }
-                    else
-                    {
-                        dayOfTheWeek++;
-                    }
+                    dayOfTheWeek = GameTime.DayOfTheWeek.MONDAY;
                 }
-            }
-            else
-            {
-                DaysWorkPerWeek = 0;
-                if (WorkFromDay != GameTime.DayOfTheWeek.NONE)
+                else
                 {
-                    DaysWorkPerWeek++;
-                }
-                if (WorkToDay != GameTime.DayOfTheWeek.NONE)
-                {
-                    DaysWorkPerWeek++;
+                    dayOfTheWeek++;
                 }
             }
 
@@ -198,20 +182,11 @@ public class JobLocation : MonoBehaviour {
             if (success)
             {
                 PlayerState.Jobs.Add(Job);
-                string message = "Congratulations! From tomorrow you work ";
-                if (Job.WorkDaysInBetween)
-                {
-                    message += GameTime.DayOfTheWeekAsShortString(Job.WorkFromDay) + " to " +
-                               GameTime.DayOfTheWeekAsShortString(Job.WorkToDay);
-                }
-                else
-                {
-                    message += GameTime.DayOfTheWeekAsShortString(Job.WorkFromDay) + " and " +
-                               GameTime.DayOfTheWeekAsShortString(Job.WorkToDay);
-                }
-                message += " from " +
-                    GameTime.GetTimeAsString(Job.ShiftFromHour) + " to " +
-                    GameTime.GetTimeAsString(Job.ShiftToHour) + ". Don't be late!";
+                string message = "Congratulations! From tomorrow you work " +
+                                 GameTime.DayOfTheWeekAsShortString(Job.WorkFromDay) + " to " +
+                                 GameTime.DayOfTheWeekAsShortString(Job.WorkToDay) + " from " +
+                                 GameTime.GetTimeAsString(Job.ShiftFromHour) + " to " +
+                                 GameTime.GetTimeAsString(Job.ShiftToHour) + ". Don't be late!";
                                 
                 MessageBox.Show(message, gameObject);
             }
