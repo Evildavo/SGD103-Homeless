ce0b8077e4e3f66d4399891cf4af01ac621d2995
Evildavo <evildavo@gmail.com>
2016-10-19 19:37:48 +1000

    Set up test scene for job searching

---
+++ SGD103-Homeless/Assets/Scripts/Triggers/ModelDiner.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ModelDiner : MonoBehaviour
+{
+    public Trigger Trigger;
+    public Menu Menu;
+
+    void Start()
+    {
+        Trigger.RegisterOnTriggerListener(OnTrigger);
+        Trigger.RegisterOnTriggerUpdateListener(OnTriggerUpdate);
+        Trigger.RegisterOnPlayerExitListener(OnPlayerExit);
+    }
+
+    public void ShowMainMenu()
+    {
+        List<Menu.Option> options = new List<Menu.Option>();
+        options.Add(new Menu.Option(OnExit, "Exit"));
+        Menu.Show(options);
+    }
+
+    void reset()
+    {
+        Menu.Hide();
+        if (Trigger)
+        {
+            Trigger.Reset();
+        }
+    }
+
+    public void OnExit()
+    {
+        reset();
+    }
+
+    public void OnTrigger()
+    {
+        ShowMainMenu();
+    }
+
+    public void OnPlayerExit()
+    {
+        reset();
+    }
+
+    public void OnTriggerUpdate()
+    {
+        // Leave menu on E key.
+        if (Input.GetKeyDown("e") || Input.GetKeyDown("enter") || Input.GetKeyDown("return"))
+        {
+            reset();
+        }
+    }
+}
