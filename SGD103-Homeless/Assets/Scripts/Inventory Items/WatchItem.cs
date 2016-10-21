7a593cc9ea5934f8ef1d93d813194ce5f334489d
Evildavo <evildavo@gmail.com>
2016-10-21 10:40:58 +1000

    Messages box messages are automatically queued now

--- SGD103-Homeless/Assets/Scripts/Inventory Items/WatchItem.cs
+++ SGD103-Homeless/Assets/Scripts/Inventory Items/WatchItem.cs
@@ -10,7 +10,7 @@ public class WatchItem : InventoryItem
 
     public override void OnPrimaryAction()
     {
-        MessageBox.ShowForTime("", CloseAfterSeconds, gameObject);
+        MessageBox.ShowForTime("", CloseAfterSeconds, gameObject, false, true);
     }
 
     void Update()
--- SGD103-Homeless/Assets/Scripts/MessageBox.cs
+++ SGD103-Homeless/Assets/Scripts/MessageBox.cs
@@ -1,11 +1,31 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
+using System.Collections.Generic;
 
 public class MessageBox : MonoBehaviour
 {
     private float fromTime;
     private float closeAfterSeconds;
+    private Queue<Message> messageQueue = new Queue<Message>();
+
+    // Message that can be queued.
+    [System.Serializable]
+    public struct Message
+    {
+        public string Content;
+        public float Duration;
+        public GameObject Source;
+        public bool IsWarning;
+
+        // A duration of zero is considered endless.
+        public Message(string content, float duration, GameObject source, bool isWarning)
+        {
+            Content = content;
+            Duration = duration;
+            Source = source;
+            IsWarning = isWarning;
+        }
+    }
 
     public Image WarningSymbol;
 
@@ -36,29 +56,62 @@ public class MessageBox : MonoBehaviour
         }
     }
 
-    // Opens the message box displaying the given message.
-    // Source is used to keep track of who is updating the message box.
-    // If the message is a warning it'll show a warning symbol.
-    public void Show(string message, GameObject source = null, bool isWarning = false)
+    // Displays the given message now.
+    void displayMessage(Message message)
     {
         gameObject.SetActive(true);
-        GetComponentInChildren<Text>().text = message;
-        closeAfterSeconds = 0;
-        Source = source;
-        IsWarning = isWarning;
+        GetComponentInChildren<Text>().text = message.Content;
+        Source = message.Source;
+        if (message.Duration != 0.0f)
+        {
+            fromTime = Time.time;
+            closeAfterSeconds = message.Duration;
+        }
+        else
+        {
+            closeAfterSeconds = 0.0f;
+        }
+        IsWarning = message.IsWarning;
         updateWarning();
     }
 
-    // Shows the message box for a number of seconds before closing.
-    public void ShowForTime(string message, float seconds, GameObject source = null, bool isWarning = false)
+    // Opens the message box displaying the given message.
+    // Source is used to keep track of who is updating the message box.
+    // If the message is a warning it'll show a warning symbol.
+    // Messages are automatically queued and displayed one after another.
+    // If force is true (default), this message goes to the top of the queue however.
+    public void Show(string message, GameObject source = null, 
+                     bool isWarning = false, 
+                     bool force = true)
     {
-        gameObject.SetActive(true);
-        GetComponentInChildren<Text>().text = message;
-        Source = source;
-        fromTime = Time.time;
-        closeAfterSeconds = seconds;
-        IsWarning = isWarning;
-        updateWarning();
+        ShowForTime(message, 0.0f, source, isWarning, force);
+    }
+
+    // Shows the message box for a number of seconds before closing.
+    // Messages are automatically queued and displayed one after another.
+    // If force is true, this message goes to the top of the queue however.
+    public void ShowForTime(string message, float seconds, 
+                            GameObject source = null, 
+                            bool isWarning = false,
+                            bool force = false)
+    {
+        // Add message to queue.
+        Message m = new Message(message, seconds, source, isWarning);
+        if (force)
+        {
+            // Override the current message.
+            displayMessage(m);
+        }
+        else
+        {
+            messageQueue.Enqueue(m);
+
+            // Display the first message if none is displayed.
+            if (!IsDisplayed() && messageQueue.Count > 0)
+            {
+                displayMessage(messageQueue.Dequeue());
+            }
+        }
     }
 
     public void Hide()
@@ -87,6 +140,12 @@ public class MessageBox : MonoBehaviour
         if (closeAfterSeconds != 0 && Time.time - fromTime > closeAfterSeconds)
         {
             Hide();
+        }
+
+        // Display the next message in the queue when current message is hidden.
+        if (!IsDisplayed() && messageQueue.Count > 0)
+        {
+            displayMessage(messageQueue.Dequeue());
         }
     }
 }
