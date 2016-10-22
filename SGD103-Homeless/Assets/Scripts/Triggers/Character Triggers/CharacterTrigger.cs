59ae46ed626c8fe5f89ec22d4ce65effc29fc22e
Evildavo <evildavo@gmail.com>
2016-10-22 10:16:15 +1000

    Created test scene for Greg (the dodgy dealer)

---
+++ SGD103-Homeless/Assets/Scripts/Triggers/Character Triggers/CharacterTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterTrigger : Trigger
+{
+    new void Start()
+    {
+        base.Start();
+    }
+
+    new void Update()
+    {
+        base.Update();
+    }
+
+    new void OnTriggerEnter(Collider other)
+    {
+        base.OnTriggerEnter(other);
+    }
+
+    new void OnTriggerExit(Collider other)
+    {
+        base.OnTriggerExit(other);
+    }
+}
---
+++ SGD103-Homeless/Assets/Scripts/Triggers/Character Triggers/Greg.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Greg : MonoBehaviour {
+
+    public CharacterTrigger CharacterTrigger;
+    
+	void Start ()
+    {
+        CharacterTrigger.RegisterOnTriggerListener(OnTrigger);
+        CharacterTrigger.RegisterOnPlayerExitListener(OnPlayerExit);
+        CharacterTrigger.RegisterOnTriggerUpdateListener(OnTriggerUpdate);
+    }
+
+    public void OnTrigger()
+    {
+        Debug.Log("Talking to Greg");
+        reset();
+    }
+
+    public void OnPlayerExit()
+    {
+        reset();
+    }
+
+    public void OnTriggerUpdate()
+    {
+        // Leave trigger on E key.
+        if (Input.GetKeyDown("e") || Input.GetKeyDown("enter") || Input.GetKeyDown("return"))
+        {
+            reset();
+        }
+    }
+
+    void reset()
+    {
+        CharacterTrigger.Reset();
+        if (CharacterTrigger)
+        {
+            CharacterTrigger.ResetWithCooloff();
+        }
+    }
+
+}
