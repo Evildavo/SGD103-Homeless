67d79ef36db0767c50f1828fbadffc159e346c4b
Evildavo <evildavo@gmail.com>
2016-10-22 15:48:36 +1000

    Added photo of player's son to the inventory

---
+++ SGD103-Homeless/Assets/Scripts/Inventory Items/SunPhotoItem.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SunPhotoItem : InventoryItem
+{
+    private bool isShowingPhoto = false;
+    private float timeAtPhotoShown;
+
+    public Image SonPhotoImage;
+
+    public float ShowPhotoForSeconds = 3.0f;
+
+    // Shows the photo of the the player character's son briefly.
+    public void ShowPhoto()
+    {
+        isShowingPhoto = true;
+        SonPhotoImage.gameObject.SetActive(true);
+        timeAtPhotoShown = Time.time;
+    }
+
+    public void HidePhoto()
+    {
+        SonPhotoImage.gameObject.SetActive(false);
+        isShowingPhoto = false;
+    }
+
+    public override void OnPrimaryAction()
+    {
+        if (!isShowingPhoto)
+        {
+            ShowPhoto();
+        }
+        else
+        {
+            HidePhoto();
+        }
+    }
+
+    void Update()
+    {
+        if (isShowingPhoto)
+        {
+            // Close the photo after time or after a button press.
+            if (Time.time - timeAtPhotoShown > ShowPhotoForSeconds)
+            {
+                HidePhoto();
+            }
+        }
+    }
+
+}
