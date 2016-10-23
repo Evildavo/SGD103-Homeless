4fa3a9ab4f1da6868a86922ce4cf1314ca76699b
Evildavo <evildavo@gmail.com>
2016-10-23 20:53:16 +1000

    Added test objective for "Get more alcohol"

--- SGD103-Homeless/Assets/Scripts/ObjectiveList.cs
+++ SGD103-Homeless/Assets/Scripts/ObjectiveList.cs
@@ -9,6 +9,7 @@ public class ObjectiveList : MonoBehaviour {
     [Header("Objectives are removed when full")]
     public int NumSlots;
 
+    // Adds a new objective with the given name to the list. Returns the created objective.
     public Objective NewObjective(string name)
     {
         // Push other objectives down one slot.
@@ -28,7 +29,8 @@ public class ObjectiveList : MonoBehaviour {
         objective.ObjectiveName = name;    
         return objective;
     }
-
+    
+    // Removes the given objective from the list.
     public void RemoveObjective(Objective objective)
     {
         // Move other objectives below it up one slot.
--- SGD103-Homeless/Assets/Scripts/Test/ObjectiveTestTrigger.cs
+++ SGD103-Homeless/Assets/Scripts/Test/ObjectiveTestTrigger.cs
@@ -1,19 +1,35 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class ObjectiveTestTrigger : MonoBehaviour {
+    private List<Objective> alcoholObjectives = new List<Objective>();
 
     public Main Main;
     public Trigger Trigger;
 
-	void Start () {
+    void Start () {
         Trigger.RegisterOnTriggerListener(onTrigger);
+        Main.Inventory.RegisterOnItemAdded(onItemAdded);
+    }
+
+    void onItemAdded(InventoryItem item)
+    {
+        // Remove an objective if we found alcohol.
+        if (item.ItemName == "Alcohol")
+        {
+            if (alcoholObjectives.Count > 0)
+            {
+                int last = alcoholObjectives.Count - 1;
+                alcoholObjectives[last].Achieved = true;
+                alcoholObjectives.RemoveAt(last);
+            }
+        }
     }
 
     void onTrigger()
     {
-        Main.ObjectiveList.NewObjective("Get more alcohol");
+        alcoholObjectives.Add(Main.ObjectiveList.NewObjective("Get more alcohol"));
         Trigger.Reset();
     }
-	
+        	
 }
