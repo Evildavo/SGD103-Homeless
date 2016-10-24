3a6bc27d54e6ba32459ca801e0cd3781d3be5565
Evildavo <evildavo@gmail.com>
2016-10-24 22:34:49 +1000

    Spawns addiction objectives while addicted

--- SGD103-Homeless/Assets/Scripts/PlayerState.cs
+++ SGD103-Homeless/Assets/Scripts/PlayerState.cs
@@ -1,10 +1,15 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class PlayerState : MonoBehaviour {
     private float timeAtLastVomit;
     private bool hasVomited = false;
+    private float timeAtLastObjectiveSpawn;
+    private bool hasSpawnedObjective = false;
+    private List<Objective> alcoholObjectives = new List<Objective>();
 
     public Main Main;
+    public List<AlcoholItem> AlcoholPrefabs;
 
     [Header("Display settings:")]
     public Color NormalTextColour = Color.black;
@@ -69,7 +74,11 @@ public class PlayerState : MonoBehaviour {
     public float StartsVomitingAtInebriation;
     public float VomitIntervalAtMaxInebriationSeconds;
     public float HungerSatietyLostPerVomit;
+
+    [Space(10.0f)]
     public float MaxAddictionMoralePenaltyPerHour;
+    public float SpawnObjectivesAboveAddictionLevel;
+    public float ObjectiveSpawnIntervalAtMaxAddictionSeconds;
 
     [Space(10.0f)]
     public bool IsAtWork = false;
@@ -87,7 +96,30 @@ public class PlayerState : MonoBehaviour {
     {
         return (Money >= price);
     }
-        
+
+    void Start()
+    {
+        Main.Inventory.RegisterOnItemAdded(onItemAdded);
+    }
+
+    void onItemAdded(InventoryItem item)
+    {
+        // Remove an objective if we found alcohol.
+        foreach (AlcoholItem alcohol in AlcoholPrefabs)
+        {
+            if (item.ItemName == alcohol.ItemName)
+            {
+                if (alcoholObjectives.Count > 0)
+                {
+                    int last = alcoholObjectives.Count - 1;
+                    alcoholObjectives[last].Achieved = true;
+                    alcoholObjectives.RemoveAt(last);
+                }
+                break;
+            }
+        }
+    }
+
     void Update () {
         float gameTimeDelta = 1.0f / 60.0f / 60.0f * Time.deltaTime * Main.GameTime.TimeScale;  
         
@@ -167,6 +199,7 @@ public class PlayerState : MonoBehaviour {
             {
                 hasVomited = true;
                 timeAtLastVomit = Time.time;
+
                 HungerThirstSatiety -= HungerSatietyLostPerVomit;
                 Main.PlayerCharacter.Vomit();
             }
@@ -174,6 +207,21 @@ public class PlayerState : MonoBehaviour {
 
         // Addiction affects morale.
         Morale -= Addiction * MaxAddictionMoralePenaltyPerHour * gameTimeDelta;
+
+        // Spawn alcohol objectives sometimes while addicted.
+        if (Addiction >= SpawnObjectivesAboveAddictionLevel)
+        {
+            float level = (Addiction - SpawnObjectivesAboveAddictionLevel) / (1.0f - SpawnObjectivesAboveAddictionLevel);
+
+            if (!hasSpawnedObjective || (level != 0.0f &&
+                Time.time - timeAtLastObjectiveSpawn > ObjectiveSpawnIntervalAtMaxAddictionSeconds / level))
+            {
+                hasSpawnedObjective = true;
+                timeAtLastObjectiveSpawn = Time.time;
+                
+                alcoholObjectives.Add(Main.ObjectiveList.NewObjective("Get more alcohol"));
+            }
+        }
 
         // Limit stats to range 0-1.
         if (Money < 0)
