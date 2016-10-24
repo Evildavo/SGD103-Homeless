7353fb18d62ced501809ae02cc5cc3d8c7dc1dff
Evildavo <evildavo@gmail.com>
2016-10-24 20:03:55 +1000

    Can drink alcohol

--- SGD103-Homeless/Assets/Scripts/Inventory Items/AlcoholItem.cs
+++ SGD103-Homeless/Assets/Scripts/Inventory Items/AlcoholItem.cs
@@ -2,10 +2,45 @@
 using System.Collections;
 
 public class AlcoholItem : MultiUseItem
-{    
+{
+    public float ThirstSatietyBenefitPerUse;
+    public float MoraleBenefitPerUse;
+    public float InebriationIncreasePerUse;
+
     public override void OnPrimaryAction()
     {
-        Debug.Log("Drank alcohol");
+        var PlayerState = Main.PlayerState;
+        var MessageBox = Main.MessageBox;
+
+        // Increase stats.
+        PlayerState.HungerThirstSatiety += ThirstSatietyBenefitPerUse;
+        PlayerState.Morale += MoraleBenefitPerUse;
+        PlayerState.Inebriation += InebriationIncreasePerUse;
+
+        // Show message depending on how inebriated the player is after drinking.
+        if (PlayerState.Inebriation > 0.8f)
+        {
+            MessageBox.ShowForTime("You feel very good", 2.0f, gameObject);
+        }
+        else if (PlayerState.Inebriation > 0.6f)
+        {
+            MessageBox.ShowForTime("You feel pretty good", 2.0f, gameObject);
+        }
+        else if (PlayerState.Inebriation > 0.4f)
+        {
+            MessageBox.ShowForTime("You feel a little tipsy", 2.0f, gameObject);
+        }
+        else
+        {
+            MessageBox.ShowForTime("You feel a bit better", 2.0f, gameObject);
+        }
+
+        // Consume item.
+        NumUses -= 1;
+        if (NumUses == 0)
+        {
+            Main.Inventory.RemoveItem(this);
+        }
     }
 
     void Update()
