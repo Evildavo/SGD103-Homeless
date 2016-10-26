2c6c372a2fd344c29802f9c30fd35e5be0407a60
Evildavo <evildavo@gmail.com>
2016-10-26 22:31:12 +1000

    Other hud buttons move to centre when the watch is gone

--- SGD103-Homeless/Assets/Scripts/UI/HUD Buttons/HudButtons.cs
+++ SGD103-Homeless/Assets/Scripts/UI/HUD Buttons/HudButtons.cs
@@ -3,18 +3,17 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class HudButtons : MonoBehaviour {
-    
+    Vector3 initialInventoryHudButtonPosition;
+    Vector3 initialSleepHudButtonPosition;
+
     public Transform HudButtonLabel;
     public InventoryHudButton InventoryHudButton;
     public InventoryHudButton CloseInventoryHudButton;
     public SleepHudButton SleepHudButton;
     public WatchHudButton WatchHudButton;
 
-    void Start()
-    {
-        HudButtonLabel.gameObject.SetActive(false);
-        OpenRegularMode();
-    }
+    [Header("For moving items to centre when watch is gone")]
+    public float MoveButtonsXWhenWatchGoneAmount = -60.0f; 
 
     // Ensures hud is disabled in correct state.
     public void Hide()
@@ -41,7 +40,42 @@ public class HudButtons : MonoBehaviour {
         CloseInventoryHudButton.gameObject.SetActive(false);
         InventoryHudButton.gameObject.SetActive(true);
         SleepHudButton.gameObject.SetActive(true);
-        WatchHudButton.gameObject.SetActive(true);
+        if (WatchHudButton.Watch)
+        {
+            WatchHudButton.gameObject.SetActive(true);
+        }
+    }
+
+    void Start()
+    {
+        initialInventoryHudButtonPosition = InventoryHudButton.transform.position;
+        initialSleepHudButtonPosition = SleepHudButton.transform.position;
+
+        HudButtonLabel.gameObject.SetActive(false);
+        OpenRegularMode();
+    }
+
+    void Update()
+    {
+        // Move other items to centre when watch gone.
+        if (!WatchHudButton.gameObject.activeInHierarchy)
+        {
+            {
+                var position = initialInventoryHudButtonPosition;
+                position.x += MoveButtonsXWhenWatchGoneAmount;
+                InventoryHudButton.transform.position = position;
+            }
+            {
+                var position = initialSleepHudButtonPosition;
+                position.x += MoveButtonsXWhenWatchGoneAmount;
+                SleepHudButton.transform.position = position;
+            }
+        }
+        else
+        {
+            InventoryHudButton.transform.position = initialInventoryHudButtonPosition;
+            SleepHudButton.transform.position = initialSleepHudButtonPosition;
+        }
     }
 
 }
