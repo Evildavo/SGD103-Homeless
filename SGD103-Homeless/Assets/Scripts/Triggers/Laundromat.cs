fe0debedddc41e7a053982a2874d404a98c27f3a
Evildavo <evildavo@gmail.com>
2016-11-04 20:21:54 +1000

    Fade to black effect while using laundromat

--- SGD103-Homeless/Assets/Scripts/Triggers/Laundromat.cs
+++ SGD103-Homeless/Assets/Scripts/Triggers/Laundromat.cs
@@ -8,6 +8,9 @@ public class Laundromat : MonoBehaviour
 
     public float CostToUse;
     public float TimeCostToUse;
+    public float FadeToBlackTime = 2.0f;
+    public float FadeInFromBlackTime = 2.0f;
+    public bool IncludeClothesPlayerIsWearing = true;
 
     public void OpenMainMenu()
     {
@@ -23,16 +26,63 @@ public class Laundromat : MonoBehaviour
         // Pay money.
         Main.PlayerState.Money -= CostToUse;
 
+        // Show washing message.
+        Main.MessageBox.Show("Washing...", gameObject);
+
+        // Fade to black.
+        Main.ScreenFader.fadeTime = FadeToBlackTime;
+        Main.ScreenFader.fadeIn = false;
+        Invoke("OnFadeOutComplete", FadeInFromBlackTime);
+
+        // Hide UI and switch to modal mode.
+        Main.UI.Hide();
+        Main.UI.EnableModalMode();
+    }
+
+    void OnFadeOutComplete()
+    {
         // Apply time cost.
         Main.GameTime.SpendTime(TimeCostToUse);
+        
+        // Wash what the player is wearing.
+        if (IncludeClothesPlayerIsWearing && Main.PlayerState.CurrentClothing)
+        {
+            Main.PlayerState.CurrentClothing.Cleanliness = 1.0f;
+        }
 
         // Wash all clothes in the player inventory.
+        bool hasClothesInInventory = false;
         foreach (ClothingItem item in Main.Inventory.ItemContainer.GetComponentsInChildren<ClothingItem>())
         {
             item.Cleanliness = 1.0f;
+            hasClothesInInventory = true;
         }
+
+        // Open the inventory to show the clean clothes.
+        if (hasClothesInInventory)
+        {
+            Main.Inventory.ShowPreview();
+        }
+
+        // Show UI and exit modal mode.
+        Main.UI.Show();
+        Main.UI.DisableModalMode();
+        
+        // Fade in from black.
+        Main.ScreenFader.fadeTime = FadeInFromBlackTime;
+        Main.ScreenFader.fadeIn = true;
+
+        // Show message that clothes were cleaned.
+        Main.MessageBox.ShowForTime("Clothes washed", null, gameObject);
+
+        Invoke("onFadeInComplete", FadeInFromBlackTime);
+        reset();
     }
-    
+
+    void onFadeInComplete()
+    {
+    }
+
     void Start ()
     {
         Trigger.RegisterOnTriggerListener(OnTrigger);
