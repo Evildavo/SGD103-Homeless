b7f3e7327dd75105c9db17b7cc30358c9395b56a
Evildavo <evildavo@gmail.com>
2016-11-05 22:32:21 +1000

    Camera can shift forward/backward based on vertical mouse movement

--- SGD103-Homeless/Assets/Scripts/ThirdPersonCamera.cs
+++ SGD103-Homeless/Assets/Scripts/ThirdPersonCamera.cs
@@ -7,6 +7,7 @@ public class ThirdPersonCamera : MonoBehaviour {
 
     public float MaxTurnAngleDegrees;
     public float MaxCameraSidewaysShift;
+    public float MaxCameraForwardsShift;
 
     void Start () {
         initialPosition = transform.localPosition;
@@ -15,15 +16,20 @@ public class ThirdPersonCamera : MonoBehaviour {
 
     void Update()
     {
-        // Turn horizontally and shift the camera sideways based on how far the mouse is from centre.
+        // Turn horizontally and shift the camera sideways based on how far the mouse is from horizontal centre.
         float halfScreenWidth = (Screen.width / 2.0f);
-        float delta = (Input.mousePosition.x - halfScreenWidth) / halfScreenWidth;
+        float horizontalDelta = (Input.mousePosition.x - halfScreenWidth) / halfScreenWidth;
         transform.localRotation =
             initialRotation * Quaternion.Euler(
                 0.0f,
-                delta * MaxTurnAngleDegrees, 
+                horizontalDelta * MaxTurnAngleDegrees, 
                 0.0f);
         transform.localPosition =
-            initialPosition + new Vector3(0.0f, delta * MaxCameraSidewaysShift, 0.0f);
-	}
+            initialPosition + new Vector3(0.0f, horizontalDelta * MaxCameraSidewaysShift, 0.0f);
+
+        // Shift the camera forward/backward based on how far the mouse is from vertical centre.
+        float halfScreenHeight = (Screen.height / 2.0f);
+        float verticalDelta = (Input.mousePosition.y - halfScreenWidth) / halfScreenWidth;
+        transform.localPosition += new Vector3(verticalDelta * MaxCameraForwardsShift, 0.0f, 0.0f);
+    }
 }
