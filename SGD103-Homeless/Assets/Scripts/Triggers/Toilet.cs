2d49a2d9c1b316ca265ec064f5028709b3891f47
David Page <dcp002@usc.edu.au>
2016-11-10 10:03:48 +1000

    Added toilet trigger

---
+++ SGD103-Homeless/Assets/Scripts/Triggers/Toilet.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Toilet : MonoBehaviour
+{
+    public Main Main;
+    public Trigger Trigger;
+    
+    public void OpenMainMenu()
+    {
+        List<Menu.Option> options = new List<Menu.Option>();
+        options.Add(new Menu.Option(OnExit, "Exit"));
+
+        Main.Menu.Show(options);
+    }
+
+    void Start ()
+    {
+        Trigger.RegisterOnTriggerListener(OnTrigger);
+        Trigger.RegisterOnCloseRequested(reset);
+    }
+
+    void OnTrigger()
+    {
+        OpenMainMenu();
+    }
+
+    void OnExit()
+    {
+        reset();
+        Main.MessageBox.ShowNext();
+    }
+    
+    void reset()
+    {
+        Main.Menu.Hide();
+        Trigger.Reset();
+    }
+
+}
