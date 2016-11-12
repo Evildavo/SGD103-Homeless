fb0d56290d596c611683c257cd3d3c6ff02b2f83
Cailean Congedo <cac029@usc.edu.au>
2016-11-13 01:48:11 +1000

    Added example dialogue for Lucy for Cailean

--- SGD103-Homeless/Assets/Scripts/Triggers/Character Triggers/Lucy.cs
+++ SGD103-Homeless/Assets/Scripts/Triggers/Character Triggers/Lucy.cs
@@ -3,6 +3,15 @@ using System.Collections;
 
 public class Lucy : Character
 {
+    enum Reputation
+    {
+        GOOD,
+        STANDARD,
+        BAD
+    }
+
+    Reputation reputation = Reputation.STANDARD;
+
     void Start()
     {
         var trigger = GetComponent<Trigger>();
@@ -18,29 +27,54 @@ public class Lucy : Character
 
     public void OnTrigger()
     {
-        Speak("Hello");
-        Main.PlayerCharacter.ShowStandardDialogueMenu(
-            "Submissive",
-            "Prideful",
-            "Selfish",
-            onResponseChosen);
+        if (reputation == Reputation.STANDARD)
+        {
+            Speak("(cheerful) Hey there chin up, we've got a big day of stuff to do! ");
+            Main.PlayerCharacter.ShowStandardDialogueMenu(
+                "1. Anything in mind?",
+                "2. That's alright... ",
+                "3. Get any money from that job you applied for yet?",
+                onResponseChosen);
+        }
+        else if (reputation == Reputation.GOOD)
+        {
+
+        }
+        else
+        {
+            Speak("Leave me alone");
+            Reset();
+        }
     }
 
     void onResponseChosen(PlayerCharacter.ResponseType response)
     {
         if (response == PlayerCharacter.ResponseType.SUBMISSIVE)
         {
-            Speak("Ok");
-            Reset();
+            Speak("Well I was thinking of heading to the library later", null);
+            AddCaptionChangeCue(3, "its always good to look for jobs and improve the resume.");
+            AddCaptionChangeCue(6, "Plus reading always used to be a good way to pass the time.");
+            SetFinishCallback(() =>
+            {
+                Main.PlayerCharacter.Speak("Okay i'll have a browse if i'm free then.", null, () =>
+                {
+                    Speak("Oh ok");
+                    Reset();
+                });
+            });
         }
         else if (response == PlayerCharacter.ResponseType.PRIDEFUL)
         {
-            Speak("Ok");
-            Reset();
+            Main.PlayerCharacter.Speak("That's alright I've got my own stuff I need to take care of", null, () =>
+            {
+                Speak("Ok");
+                Reset();
+            });
         }
         else if (response == PlayerCharacter.ResponseType.SELFISH)
         {
-            Speak("Ok");
+            Speak("Well fuck you then!");
+            reputation = Reputation.BAD;
             Reset();
         }
         else
