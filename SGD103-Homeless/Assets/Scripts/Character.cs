7ae07d61e184bb0c135eaca99aecaa23a9e7f9b9
Cailean Congedo <cac029@usc.edu.au>
2016-11-13 01:47:58 +1000

    Added late callback setting for character dialogue

--- SGD103-Homeless/Assets/Scripts/Character.cs
+++ SGD103-Homeless/Assets/Scripts/Character.cs
@@ -93,6 +93,12 @@ public class Character : MonoBehaviour
             audioPositionSeconds + calculateDialogueLength(text, audioClip));
     }
 
+    // Sets the function to call when the speach finishes.
+    public void SetFinishCallback(OnFinishedSpeaking callback)
+    {
+        onFinishedCallback = callback;
+    }
+
     float calculateDialogueLength(string text, AudioClip audio)
     {
         var dialogue = Main.DialogueManager; 
@@ -241,6 +247,7 @@ public class Character : MonoBehaviour
         if (onFinishedCallback != null)
         {
             onFinishedCallback();
+            onFinishedCallback = null;
         }
     }
 
