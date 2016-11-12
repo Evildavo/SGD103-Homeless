3eeecd6ba8d9efa2604550e0d674b1a276492d57
David Page <dcp002@usc.edu.au>
2016-11-12 13:17:33 +1000

    Added lucy to the scene

---
+++ SGD103-Homeless/Assets/Scripts/Triggers/Character Triggers/Lucy.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lucy : Character
+{
+    public AudioClip HelloAudio;
+
+    void Start()
+    {
+        var trigger = GetComponent<Trigger>();
+        trigger.RegisterOnTriggerListener(OnTrigger);
+        trigger.RegisterOnPlayerExitListener(OnPlayerExit);
+        trigger.RegisterOnCloseRequested(Reset);
+    }
+
+    new void Update()
+    {
+        base.Update();
+    }
+
+    public void OnTrigger()
+    {
+        Speak("Hello");
+        Main.PlayerCharacter.ShowStandardDialogueMenu(
+            "Submissive",
+            "Prideful",
+            "Selfish",
+            onResponseChosen);
+    }
+
+    void onResponseChosen(PlayerCharacter.ResponseType response)
+    {
+        if (response == PlayerCharacter.ResponseType.SUBMISSIVE)
+        {
+            Speak("Ok");
+            Reset();
+        }
+        else if (response == PlayerCharacter.ResponseType.PRIDEFUL)
+        {
+            Speak("Ok");
+            Reset();
+        }
+        else if (response == PlayerCharacter.ResponseType.SELFISH)
+        {
+            Speak("Ok");
+            Reset();
+        }
+        else
+        {
+            Reset();
+        }
+    }
+
+    public void OnPlayerExit()
+    {
+        if (IsSpeaking)
+        {
+            Speak("Hey, where are you going?");
+        }
+    }
+
+    public void Reset()
+    {
+        Main.Menu.Hide();
+        GetComponent<Trigger>().Reset();
+    }
+}
