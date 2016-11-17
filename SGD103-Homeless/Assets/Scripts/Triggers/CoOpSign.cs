99d1f1cff17e00cfce8ba6c1c0bc1e6131a1dd3f
Evildavo <evildavo@gmail.com>
2016-11-18 01:28:14 +1000

    Added sign board out front of co op to show current events

--- SGD103-Homeless/Assets/Scripts/Triggers/CoOpSign.cs
+++ SGD103-Homeless/Assets/Scripts/Triggers/CoOpSign.cs
@@ -6,15 +6,47 @@ public class CoOpSign : MonoBehaviour
     public Main Main;
     public Trigger Trigger;
     public CoOpShelter CoOpShelter;
+    public MeshRenderer Face;
+    public Material FaceNone;
+    public Material FaceSoupKitchen;
+    public Material FaceTherapy;
+    public Material FaceAddictSupport;
 
     void Start()
     {
         Trigger.RegisterOnTriggerListener(OnTrigger);
+        Trigger.RegisterOnCloseRequested(Reset);
+    }
+
+    void Update()
+    {
+        if (CoOpShelter.SoupKitchenEvent.IsOpen)
+        {
+            Face.material = FaceSoupKitchen;
+        }
+        else if (CoOpShelter.CounsellingEvent.IsOpen)
+        {
+            Face.material = FaceTherapy;
+        }
+        else if (CoOpShelter.AddictionSupportEvent.IsOpen)
+        {
+            Face.material = FaceAddictSupport;
+        }
+        else
+        {
+            Face.material = FaceNone;
+        }
     }
 
     public void OnTrigger()
     {
         CoOpShelter.ReadNoticeBoard();
+    }
+
+    public void Reset()
+    {
+        Main.MessageBox.ShowNext();
+        Trigger.Reset();
     }
     
 }
